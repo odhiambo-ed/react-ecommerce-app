22683af7d8644e5c53b5584172d23a5641bc02c0
white3d <odhiambo.edward111@gmail.com>
2022-11-21 09:58:39 +0300

    Add Favorites Reducers

---
+++ src/features/slices/favoritesSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import db from '../../firebase';
+
+export const favoritesSlice = createSlice({
+    name: 'favorites',
+    initialState: {
+        value: [],
+    },
+    reducers: {
+        getFavoritesData: (state, action) => {
+            state.value = [...action.payload];
+        },
+        addToFavorites: (state, action) => {
+            db.collection('favoritesPlacement').add(action.payload);
+        },
+        removeFromFavorites: (state, action) => {
+            db.collection('favoritesPlacement').doc(action.payload).delete();
+        },
+    },
+});
+
+export const { getFavoritesData, addToFavorites, removeFromFavorites } = favoritesSlice.actions;
+
+export default favoritesSlice.reducer;
