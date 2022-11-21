00bede8cd3b040f302d0ffa0e5359c4cc4c4b0fb
white3d <odhiambo.edward111@gmail.com>
2022-11-21 09:50:39 +0300

    Add Favorites JS Component

---
+++ src/components/Favorites.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useSelector } from "react-redux";
+
+import FavoritesCard from "./FavoritesCard";
+
+const Favorites = () => {
+    const data = useSelector((state) => state.favorites.value);
+    return (
+        <Container
+            style={{
+                marginTop: 100,
+            }}
+        >
+            <Row>
+                {data.map((item) => {
+                    return (
+                        <Col sm key={item.id}>
+                            <FavoritesCard
+                                id={item.id}
+                                name={item.data.itemName}
+                                image={item.data.itemImage}
+                                price={item.data.price}
+                                summary={item.data.summary}
+                                description={item.data.description}
+                                quantity={item.data.quantity}
+                            />
+                        </Col>
+                    );
+                })}
+            </Row>
+        </Container>
+    );
+};
+
+export default Favorites;
