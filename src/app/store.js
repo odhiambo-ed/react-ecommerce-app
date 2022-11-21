2c85efa3afe5d5ff4c1a3e70c3d6b4779b3de505
white3d <odhiambo.edward111@gmail.com>
2022-11-21 09:56:43 +0300

    Add Redux Store

---
+++ src/app/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import authReducer from '../features/slices/authSlice';
+import cartReducer from '../features/slices/cartSlice';
+import favoritesReducer from '../features/slices/favoritesSlice';
+import purchasesReducer from '../features/slices/purchasesSlice';
+
+export default configureStore({
+    reducer: {
+        auth: authReducer,
+        cart: cartReducer,
+        favorites: favoritesReducer,
+        purchases: purchasesReducer,
+    },
+});
