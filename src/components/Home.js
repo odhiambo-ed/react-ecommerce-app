39141bd9f4901cae1726411951d1396f9dc7dede
white3d <odhiambo.edward111@gmail.com>
2022-11-21 09:51:35 +0300

    Add Home JS Component

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import Navigation from "./Navigation";
+import Items from "./Items";
+import Favorites from "./Favorites";
+import Cart from "./Cart";
+import Purchases from "./Purchases";
+
+const Home = () => {
+  const [currentTab, setCurrentTab] = useState("Home");
+  return (
+    <div>
+      <Navigation setCurrentTab={setCurrentTab} />
+      {currentTab === "Home" ? (
+        <Items />
+      ) : currentTab === "Favorites" ? (
+        <Favorites />
+      ) : currentTab === "Purchases" ? (
+        <Purchases />
+      ) : (
+        <Cart />
+      )}
+    </div>
+  );
+};
+
+export default Home;
