4dea4c762fef7cf5f524af0fe67b55fe34db1f02
white3d <odhiambo.edward111@gmail.com>
2022-11-21 09:58:05 +0300

    Add Firebase Auth Reducer

---
+++ src/features/slices/authSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { auth, provider } from '../../firebase';
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        value: 0,
+    },
+    reducers: {
+        signIn: () => {
+            auth.signInWithPopup(provider);
+        },
+        signOut: () => {
+            auth.signOut();
+        },
+    },
+});
+
+export const { signIn, signOut } = authSlice.actions;
+
+export default authSlice.reducer;
