c446327f8bd4d2f8b250ce5a46c10370544e330a
white3d <odhiambo.edward111@gmail.com>
2022-11-21 09:58:20 +0300

    Add Cart Reducers

---
+++ src/features/slices/cartSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import db from '../../firebase';
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        value: [],
+    },
+    reducers: {
+        getData: (state, action) => {
+            state.value = [...action.payload];
+        },
+        addToCart: (state, action) => {
+            db.collection('cartPlacement').add(action.payload);
+        },
+        removeFromCart: (state, action) => {
+            db.collection('cartPlacement').doc(action.payload).delete();
+        },
+        updateCart: (state, action) => {
+            db.collection('cartPlacement').doc(action.payload.id).update(action.payload.data)
+        }
+    },
+});
+
+export const { getData, addToCart, removeFromCart, updateCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
